ce311b39d28eef0e819fc97a0c633633871941f7
talandar <talandar66@protonmail.com>
2024-02-18 18:19:44 +0100

    added keybind for horizontal window resizing

--- config.el
+++ config.el
@@ -84,6 +84,10 @@
 (define-key evil-visual-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
 (define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
 (define-key evil-visual-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+
+(define-key evil-normal-state-map (kbd "C-M-=") 'enlarge-window-horizontally)
+(define-key evil-normal-state-map (kbd "C-M--") 'shrink-window-horizontally)
+
 ;; use fucking editorconfig
 (setq lsp-enable-indentation nil)
 (editorconfig-mode 1)
